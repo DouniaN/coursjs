ce179811e8b74065292f5c4e779a07b025873543
DouniaN <nougaouidounia.dev@gmial.com>
2022-10-28 09:00:51 +0200

    Exercice conditions

--- api.js
+++ api.js
@@ -1,31 +1,32 @@
-// // Option fonction anonyme
-// (async() => {
-// const contenu = await fetch("https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_stations-velos-libre-service-nantes-metropole-disponibilites&q=&facet=banking&facet=bonus&facet=status&facet=contract_name");
-// const contenuJson = await contenu.json()
-// console.log(contenuJson)
+// Option fonction anonyme
+(async() => {
+const contenu = await fetch("https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_stations-velos-libre-service-nantes-metropole-disponibilites&q=&facet=banking&facet=bonus&facet=status&facet=contract_name");
+const contenuJson = await contenu.json()
+console.log(contenuJson)
 
-// })()
+})()
 
-function estAnagrame(stringA ,stringB){
-    if (stringA.length !== stringB.length) {
-        // not the same length, can't be anagram
-        return false;
-    }
 
-    //Triez les deux chaînes.
-    var s1 = stringA.split('').sort().join('');
-    var s2 = stringB.split('').sort().join('');
+// function estAnagrame(stringA ,stringB){
+//     if (stringA.length !== stringB.length) {
+//         // not the same length, can't be anagram
+//         return false;
+//     }
 
-    //Comparer les deux chaines 
-    if(stringA===stringB){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-//Test function
-console.log(estAnagrame('abc','cba'));
+//     //Triez les deux chaînes.
+//     var s1 = stringA.split('').sort().join('');
+//     var s2 = stringB.split('').sort().join('');
+
+//     //Comparer les deux chaines 
+//     if(stringA===stringB){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// //Test function
+// console.log(estAnagrame('abc','cba'));
 
   
 
--- cours.js
+++ cours.js
@@ -1,5 +1,31 @@
-alert("hello git");
 
-//git pull recupérer les informations depuis github à local
+//Exercice if 
+let douleur = prompt("Bonjour ou avez vous mal monsieur !");
+if(douleur =="Abdomen"){
+ alert("Apendicite")
+}
+if(douleur=="Gorge"){
+let fievre=prompt("Avez vous de fievre !");
+ if(fievre=="Oui"){
+    alert("Rhume");
+ }
+ else if(fievre=="Non"){
+    alert("Mal de gorge");
+ } 
+}
 
-alert("git pull");
+if(douleur!="Abdomen"){
+    let toux=prompt("Avez vous du la Toux ??").toLowerCase();
+    if(toux=="oui"){
+        let fievre =prompt("Avez vous de fievre ??");
+        if(fievre=="oui"){
+            alert("Monsieur vous avez du Rhume");
+        }
+        else if(fievre=="non"){
+            alert("Refroidissement");
+        }
+    }
+    else{
+        alert("Rien");
+    }
+}
