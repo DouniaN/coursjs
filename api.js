c5a4a0992f83342157de75c6b54a44af49bf5972
DouniaN <nougaouidounia.dev@gmial.com>
2022-10-28 09:06:33 +0200

    Exercice api

--- api.js
+++ api.js
@@ -2,12 +2,12 @@
 (async() => {
 const contenu = await fetch("https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_stations-velos-libre-service-nantes-metropole-disponibilites&q=&facet=banking&facet=bonus&facet=status&facet=contract_name");
 const contenuJson = await contenu.json()
-console.log(contenuJson)
+console.log(contenuJson);
 
 })()
 
 
-
+   
 
   
 
