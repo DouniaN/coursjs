8345c0830a79dfdf97bf8d8edde701176c638eac
DouniaN <nougaouidounia.dev@gmial.com>
2022-10-28 09:09:13 +0200

    Exercice Function

--- bouclejs.js
+++ bouclejs.js
@@ -1,25 +1,5 @@
 
-       
-      /*for (let i =1;i<=199; i++){
-        document.write(i+'<br/>');
-        if(i%3==0 && i%5!=0){
-            console.log("coucou !<br/>");
-        }
-        if(i%5==0 && i%3!=0){
-            console.log("hello <br/>");
-        }
-        if(i%3==0 && i%5==0){
-            console.log("couco hello <br/>");
-        }
-
-      } 
-
-      let maVariableNull = null;
-      let mavaribleIndefinde ;
-
-      console.log(typeof(mavaribleIndefinde));
-      console.log(typeof(maVariableNull));*/
-   /*********************************************Fonctions************************************************************** */
+/*********************************************Fonctions************************************************************** */
 
    /*function afficherDeuxValeurs(param1) {
       return param1 ;
