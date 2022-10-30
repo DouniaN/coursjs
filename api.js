8834afe3ed7ebfb6de1c4ad2839dc9bd60eea6ec
DouniaN <nougaouidounia.dev@gmial.com>
2022-10-30 12:56:00 +0100

    Exec api

--- api.js
+++ api.js
@@ -1,60 +1,60 @@
 // Option fonction anonyme
-const data=(async() => {
-  const contenu = await fetch("https://ghibliapi.herokuapp.com/films");
+const api_url ="https://ghibliapi.herokuapp.com/films";
+
+async function getApi(url){
+  const contenu = await fetch(url);
   const contenuJson = await contenu.json()
-  console.log(contenuJson) ;
-  
-  
-      let tab = 
-       `<tr>
-       <th>title</th>
-       <th>original_title</th>
-        <th>image</th>
-       <th>description</th>
-        </tr>`;
-  
-      for (let r of contenuJson){  
+  console.log(contenuJson);
+  if(contenuJson){
+    hideloader(); 
+  }
+  showData(contenuJson);
+  getPeople(contenuJson);
+}
+
+
+getApi(api_url);
+
+
+//Function to hide the loader
+function hideloader() {
+  document.getElementById('loading').style.display = 'none';
+}
+
+function showData(data){
+  let tab =
+     `<tr>
+      <th>title</th>
+      <th>original_title</th>
+      <th>image</th>
+      <th>description</th>
+      </tr>`;
+
+      for (let r of data){
+        tab+=
+         `<tr>
+         <td>${r.title}<td>
+         <td>${r.original_title}</td>
+         <td>${r.image}</td> 
+         <td>${r.description}</td>  
+         </tr> `;
         
-          tab += `<tr> 
-          <td>${r.title} </td>
-          <td>${r.original_title}</td>
-          <td>${r.image}</td> 
-          <td>${r.description}</td>          
-          </tr>`;  
-          for(p of r.people){
-              const contenu = await fetch(p);
-              const contenuJson = await contenu.json()
-              console.log(contenuJson) 
-          }
-          
-         
-  
-        //console.log("Les id : " + r.id)
       }
-  
       document.getElementById("films").innerHTML = tab;
-  })()
-  
-  
-  //Function to define innerHTML for HTML table
-  //  function show(data) {
-  //     let tab = 
-  //     `<tr>
-  //         <th>title</th>
-  //         <th>original_title</th>
-  //         <th>image</th>
-  //         <th>description</th>
-  //     </tr>`;
-      
-  //    // Loop to access all rows 
-  //      for (let r of data.PromiseResult) {
-  //         tab += `<tr> 
-  //         <td>${r.title} </td>
-  //         <td>${r.original_title}</td>
-  //         <td>${r.image}</td> 
-  //         <td>${r.description}</td>          
-  //         </tr>`;
-  //     }
-  //     // Setting innerHTML as tab variable
-  //     document.getElementById("employees").innerHTML = tab;
-  // }
+}
+
+async function getPeople(data){
+   
+   for(let r of data){
+      for(let p of r.people){
+        const contenu = await fetch(p);
+        const people = await contenu.json()
+
+        console.log(people.name);
+          
+          
+      }
+   }
+
+}
+
