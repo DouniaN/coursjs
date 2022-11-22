8925cb1a49507fe1a6170ed118bd896fef488aaa
DouniaN <nougaouidounia.dev@gmial.com>
2022-11-22 17:20:33 +0100

    modification

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+
+const contacts = [{
+  nom:"Nougaoui",
+  telephone :"Dounia"
+
+},
+
+{nom : "manal",
+telephone :"Dounia" }
+
+]
+
+app.get('/', function (req, res) {
+    res.send('hello world')
+})
+
+app.get('/api/contacts', function (req, res) {
+    res.status(200).json(contacts)
+})
+  
